ee9c9e6205969af668657a2f2454a946a2083230
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-08-08 18:34:10 -0400

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
+import Header from "./components/Header"
+
 function App(){
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const loading = false
-    const showComments = true
-
-    if (loading) return <h1>loading...</h1>
+    
 
     return (
-    <div className="container">
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-
-        {showComments && (<div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index)=>(
-                    <li key={index}>{comment.text}</li>
-                    ))}
-            </ul>
-        </div>) }
-
-        
-    </div>
-    )
+        <>
+            <Header/>
+            <div className="container">
+            <h1>My App</h1>
+            </div>
+        </>
+        ) 
 }
 export default App
