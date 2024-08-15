4dda1c3b73735c48d0bf34f126000d61d9830bf7
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-08-15 09:37:20 -0400

    Adding a new feedback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FeedbackStats from "./components/FeedbackStats"
 import Header from "./components/Header"
 import FeedbackData from './data/FeedbackData'
 import { useState } from "react"
+import {v4 as uuidv4} from 'uuid'
 
 function App(){
     
@@ -19,11 +20,19 @@ const deleteFeedback = (id) => {
 const changeColors = () =>{
     setReverse((prev) => !prev);
 }
+
+const addFeedback = (newFeedback) => {
+    newFeedback.id = +uuidv4()
+    setFeedback([newFeedback,...feedback])
+
+}
+
+
     return (
         <>
             <Header handleReverse={changeColors}/>
             <div className="container">
-            <FeedbackForm reverse={reverse}/>
+            <FeedbackForm reverse={reverse} handleAdd={addFeedback}/>
             <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback}
             handleDelete={deleteFeedback}
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Card from './shared/Card'
 import { useState } from 'react'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 
-function FeedbackForm( {reverse} ) {
+function FeedbackForm( {reverse, handleAdd} ) {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
@@ -24,11 +26,22 @@ function FeedbackForm( {reverse} ) {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback =  {
+                text,
+                rating
+            }
+            handleAdd(newFeedback)
+        }
+    }
 
   return (
     <Card reverse={reverse} >
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select ={(rating)=> setRating(rating)}/>
         <div className='input-group'>
             <input
             onChange={handleTextChange}
