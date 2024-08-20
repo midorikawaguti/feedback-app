73df3ae348b015f67a859aac433dcdcaa326df8e
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-08-20 14:40:28 -0400

    Edit FeedbackEvent and useEffect

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,22 +23,39 @@ export const FeedbackProvider = ({children})=>{
             },
         ])
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item:{},
+        edit: false
+    })
+
+    //Add Feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
         setFeedback([newFeedback, ...feedback]);
     };
 
+    //Delete Feedback
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id));
         }
     };
 
+    //Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
+
 
     return( <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
-        addFeedback
+        addFeedback, 
+        editFeedback,
+        feedbackEdit
     }}>
         {children}
     </FeedbackContext.Provider>
